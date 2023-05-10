1c67589a2a381d7e957ccbf89397abd015f83c06
atomoffcode <kamilka@code.edu.az>
2023-05-10 11:49:04 +0400

    AddedSomeFonts

--- src/Styles/Style.tsx
+++ src/Styles/Style.tsx
@@ -5,9 +5,9 @@ export const Style = () => {
   return (
     <div >
 
-        <h1 className='mainfont font24 bugFilled-false'>Style</h1>
-        <h1 className='mainfont2 fontsemibold font40 Arrowd-false'>Style</h1>
-        <h1 className='mainfont2 fontsemibold font40 infoFill-false'>Style</h1>
+        <h1 className='main-font font-24 bugFilled-false'>Style</h1>
+        <h1 className='second-font font-semibold font-40 '>Style</h1>
+        <h1 className='second-font font-semibold font-40 '>Style</h1>
     </div>
   )
 }
