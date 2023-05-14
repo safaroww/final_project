800992fab28f4c1b4918cca3c6ec662f3c45e342
atomoffcode <kamilka@code.edu.az>
2023-05-14 23:57:31 +0400

    Changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import './App.scss';
+import { Header } from './Header/Header';
 import { Style } from './Styles/Style';
 import React from 'react';
 
 function App() {
   return (
     <>
+      <Header/>
       <Style />
     </>
   );
