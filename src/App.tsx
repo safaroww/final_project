68899356e467c0bb611fc3be260a0f94230f6da1
atomoffcode <kamilka@code.edu.az>
2023-05-14 23:59:48 +0400

    InitialCreate

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.scss';
 import { Style } from './Styles/Style';
+import { Header } from './Header/Header';
 
 function App() {
   return (
    <>
+   <Header />
     <Style />
    </>
   );
--- src/Header/Header.tsx
+++ src/Header/Header.tsx
@@ -1,7 +1,30 @@
-import React from 'react'
-
+import React from 'react';
+import FlightIcon from '@mui/icons-material/Flight';
+import BedIcon from '@mui/icons-material/Bed';
+import './Header.scss';
+import Logo from '../Logo/Logo.svg';
 export const Header = () => {
-  return (
-    <div>Header</div>
-  )
-}
+  return <>
+    <div className="header">
+      <div className="header__wrapper">
+        <div className="header__nav">
+          <a href='/#' className="nav__item">
+            <FlightIcon />
+            <p className='second-font font-semibold'>Find Flight</p>
+          </a>
+          <a href='/#' className="nav__item nav__active">
+            <BedIcon />
+            <p className='second-font font-semibold'>Find Stays</p>
+          </a>
+        </div>
+        <div className="header__logo">
+          <img className='logo' src={Logo} alt="Logo" />
+        </div>
+        <div className="header__acc">
+          <a href="/#" className='btn__light second-font font-semibold'>Login</a>
+          <a className='btn__dark second-font font-semibold' href="/#">Sign up</a>
+        </div>
+      </div>
+    </div>
+  </>;
+};
