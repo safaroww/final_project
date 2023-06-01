f185907d29f194c9857aabb79f7e785fd3868e63
atomoffcode <kamilka@code.edu.az>
2023-06-01 16:47:22 +0400

    BookingHalf
    70Percent Complete

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,15 @@ import React from 'react';
 import './App.scss';
 // import { Style } from './Styles/Style';
 import { Header } from './Header/Header';
+import { HotelBookingDetail } from './pages/Hotel/HotelBookingDetail/HotelBookingDetail';
 
 function App() {
   return (
    <>
    <Header />
     {/* <Style /> */}
+    <HotelBookingDetail />
+
    </>
   );
 }
--- src/pages/Hotel/HotelBookingDetail/HotelBookingDetail.tsx
+++ src/pages/Hotel/HotelBookingDetail/HotelBookingDetail.tsx
@@ -1,6 +1,101 @@
-
-
+import React from 'react'
+import './HotelBookingDetail.scss';
+import ChevronRightIcon from '@mui/icons-material/ChevronRight';
+import Img from './media/Frame 186.png';
+import Img2 from './media/download 1.png';
+import ApartmentIcon from '@mui/icons-material/Apartment';
+import LocationOnIcon from '@mui/icons-material/LocationOn';
 export const HotelBookingDetail = () => {
-    return <h1>HotelBookingDetail</h1>;
-  };
-  
+  return (
+    <>
+      <div className="booking__detail">
+        <div className="booking__detail__path">
+          <p className='font-14'>Turkey</p>
+          <ChevronRightIcon className='font-14' />
+          <p className='font-14'>Istanbul</p>
+          <ChevronRightIcon className='font-14' />
+          <p className='font-14 current__path'>CVK Park Bosphorus Hotel Istanbul</p>
+        </div>
+        <div className="booking__detail__wrapper">
+          <div className="booking__detail__options ">
+            <div className="booking__detail__box hotel__room__option">
+              <div className="hotel__room__detail">
+                <p className="hotel__room font-24 font-bold main-font">Superior room - 1 double bed or 2 twin beds</p>
+                <p className="hotel__room__price font-32 font-bold">$<span>240</span><span className='price__per__day font-14 font'>/night</span></p>
+              </div>
+              <div className="hotel__detail">
+                <img src={Img2} alt="" className="hotel__logo" />
+                <div className="hotel__info">
+                  <p className="hotel__name font-semibold font-24">CVK Park Bosphorus Hotel Istanbul</p>
+                  <p className="hotel__add font-14 font-medium"><LocationOnIcon className='loc__ico'/> Gümüssuyu Mah. Inönü Cad. No:8, Istanbul 34437</p>
+                </div>
+              </div>
+              <div className="hotel__date__detail">
+                <div className="hdd__checkin">
+                  <p className="check__date font-semibold font-20"><span>Thursday</span>, <span>Dec 8</span></p>
+                  <p className='font-medium font-14'>Check-in</p>
+                </div>
+                <div className="hdd__middle">
+                  <span></span>
+                  <ApartmentIcon className="ap__ico"/>
+                  <span></span>
+                </div>
+                <div className="hdd__checkout">
+                  <p className="check__date font-semibold font-20"><span>Friday</span>, <span>Dec 9</span></p>
+                  <p className='font-medium font-14'>Check-out</p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="booking__detail__total">
+            <div className="booking__detail__box booking__total">
+              <div className="booking__total__minidetail">
+                <div className="btm__img">
+                  <img src={Img} alt="" />
+                </div>
+                <div className="btm__details">
+                  <p className="hotel__name second-font font-16 font-medium">CVK Park Bosphorus...</p>
+                  <p className="hotel__room__detail second-font font-20 font-semibold">Superior room - 1 double bed or 2 twin beds</p>
+                  <div className="hotel__rating">
+                    <div className="hotel__rating__number font-12 second-font font-medium">4.2</div>
+                    <p className="hotel__rating__word second-font font-12 font-bold">Very Good</p>
+                    <p className="hotel__rating__review font-12 second-font font-semibold"><span>54</span>reviews</p>
+                  </div>
+                </div>
+              </div>
+              <div className="booking__total__ad">
+                <p className='second-font font-16 font-semibold'>Your booking is protected by <span className='font-bold'>golobe</span></p>
+              </div>
+              <div className="booking__total__prices">
+                <p className="btp__header font-16 font-bold ">Price Details</p>
+                <div className="btp__basefare">
+                  <p className='font-medium font-16'>Base Fare</p>
+                  <p className='font-semibold font-16'>$<span>240</span></p>
+                </div>
+                <div className="btp__discount">
+                  <p className='font-medium font-16'>Discount</p>
+                  <p className='font-semibold font-16'>$<span>0</span></p>
+                </div>
+                <div className="btp__taxes">
+                <p className='font-medium font-16'>Taxes</p>
+                  <p className='font-semibold font-16'>$<span>20</span></p>
+                </div>
+                <div className="btp__service">
+                <p className='font-medium font-16'>Service Fee</p>
+                  <p className='font-semibold font-16'>$<span>5</span></p>
+                </div>
+                <div className="btp__total">
+                  <p className='font-medium font-16'>Total</p>
+                  <p className='font-semibold font-16'>$<span>265</span></p>
+                </div>
+              </div>
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </>
+  )
+}
+
+
